5aca66ac543e4e59389df28a6762f62eaf79fed8
James Moore <james@restphone.com>
2014-01-29 17:07:45 -0800

    first commit

---
+++ test.host/Test.fs
@@ -0,0 +1,12 @@
+﻿
+namespace test.host
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type Test() = 
+        [<Test>]
+        abstract TestCase : unit -> unit
+        [<Test>]
+        default this.TestCase  () =
+            ()
---
+++ ui.ios/ui_iosViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace ui.ios
+{
+	public partial class ui_iosViewController : UIViewController
+	{
+		public ui_iosViewController () : base ("ui_iosViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
