baf4af7d4918fe4957c26749b4a5ce25d086099e
James Moore <james@restphone.com>
2014-02-03 15:16:17 -0800

    j

--- core.host/ServiceSupport.fs
+++ core.host/ServiceSupport.fs
@@ -1,9 +1,16 @@
-﻿type ServiceInterface<'T, 'U> = 
+﻿namespace com.example
+
+#nowarn "40"
+
+open System
+
+type ServiceInterface<'T, 'U> = 
     abstract Actor : MailboxProcessor<'T> with get
     abstract Event : IEvent<'U> with get
 
 type TimerCommand =
+    | SetTimerView of ITimerView
     | Tick of double
 
-and TimerView =
+and ITimerView =
     abstract SetTime: double -> unit
--- core.host/TimerService.fs
+++ core.host/TimerService.fs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace com.example
+
+#nowarn "40"
+
+open System
+
+type TimerService() =
+    let rec waitingForView (mbox : MailboxProcessor<TimerCommand>) : Async<unit> = 
+      async {
+        let! msg = mbox.Receive()
+        match msg with
+        | Tick(x) ->
+            return! waitingForView mbox
+        | SetTimerView v ->
+            return! serving mbox v
+      }
+    and serving (mbox : MailboxProcessor<TimerCommand>) (view: ITimerView) : Async<unit> =
+      async {
+        let! msg = mbox.Receive()
+        match msg with
+        | Tick(x) ->
+            return! waitingForView mbox
+        | SetTimerView v ->
+            return! serving mbox v
+      }
+    and actor: MailboxProcessor<TimerCommand> = MailboxProcessor.Start(waitingForView)
