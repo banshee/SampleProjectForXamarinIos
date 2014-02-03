02b9b55fb3ed0a1f60c3762ed7829c5bee513b5a
James Moore <james@restphone.com>
2014-02-03 15:09:40 -0800

    j

---
+++ core.host/ServiceSupport.fs
@@ -0,0 +1,9 @@
+﻿type ServiceInterface<'T, 'U> = 
+    abstract Actor : MailboxProcessor<'T> with get
+    abstract Event : IEvent<'U> with get
+
+type TimerCommand =
+    | Tick of double
+
+and TimerView =
+    abstract SetTime: double -> unit
